c6f3480358c3268878cfc04e69758513ea32e448
牛聪 <niucong@julong.cc>
2018-04-04 08:52:25 +0800

    优化首页和列表页面

--- app/src/main/java/com/niucong/lkprinter/RecordListActivity.java
+++ app/src/main/java/com/niucong/lkprinter/RecordListActivity.java
@@ -15,24 +15,18 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.niucong.lkprinter.db.HotelCheckDB;
-import com.niucong.lkprinter.view.NiftyDialogBuilder;
-import com.niucong.lkprinter.view.wheel.DateTimeSelectView;
 
 import org.litepal.crud.DataSupport;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -60,14 +54,17 @@ public class RecordListActivity extends AppCompatActivity {
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
         mRecyclerView = (RecyclerView) findViewById(R.id.record_list_rv);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        showToday();
         mRecyclerView.addItemDecoration(new DividerItemDecoration(this,
                 DividerItemDecoration.VERTICAL));
-
         mRecyclerView.requestFocus();
+
+//        try {
+//            selectData(ymd.parse(ymd.format(new Date())), new Date());
+//        } catch (ParseException e) {
+//            setAdapter(mDatas);
+//        }
 
         et_search = (EditText) findViewById(R.id.et_search);
         et_search.addTextChangedListener(new TextWatcher() {
@@ -92,15 +89,6 @@ public class RecordListActivity extends AppCompatActivity {
         });
     }
 
-    private void showToday() {
-        try {
-            selectData(ymd.parse(ymd.format(new Date())), new Date());
-        } catch (ParseException e) {
-            mDatas = new ArrayList<>();
-        }
-        setAdapter(mDatas);
-    }
-
     private void setAdapter(List<HotelCheckDB> srs) {
         if (srs == null) {
             srs = new ArrayList<>();
@@ -109,14 +97,16 @@ public class RecordListActivity extends AppCompatActivity {
     }
 
     private void selectData(Date st, Date et) {
+        Log.d(TAG, "st=" + ymdhms.format(st) + ",et=" + ymdhms.format(st));
         mDatas = DataSupport.where("out >= ? and out <= ?", "" + st.getTime(), "" + et.getTime()).find(HotelCheckDB.class);
+        setAdapter(mDatas);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        this.getMenuInflater().inflate(R.menu.menu_select, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        this.getMenuInflater().inflate(R.menu.menu_select, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -125,93 +115,91 @@ public class RecordListActivity extends AppCompatActivity {
             case android.R.id.home:
                 this.finish(); // back button
                 break;
-            case R.id.action_all:
-                mDatas = DataSupport.findAll(HotelCheckDB.class);
-                setAdapter(mDatas);
-                break;
-            case R.id.action_day:
-                try {
-                    selectData(ymd.parse(ymd.format(new Date())), new Date());
-                    setAdapter(mDatas);
-                } catch (ParseException e) {
-                    mDatas = new ArrayList<>();
-                }
-                break;
-            case R.id.action_mouth:
-                String startDate = "";
-                try {
-                    Calendar c = Calendar.getInstance();
-                    if (c.get(Calendar.MONTH) < 9) {
-                        startDate = c.get(Calendar.YEAR) + "-0" + (c.get(Calendar.MONTH) + 1) + "-01";
-                    } else {
-                        startDate = c.get(Calendar.YEAR) + "-" + (c.get(Calendar.MONTH) + 1) + "-01";
-                    }
-                    selectData(ymd.parse(startDate), new Date());
-                    setAdapter(mDatas);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                break;
-            case R.id.action_select:
-                showSubmitDia();
-                break;
+//            case R.id.action_all:
+//                mDatas = DataSupport.findAll(HotelCheckDB.class);
+//                setAdapter(mDatas);
+//                break;
+//            case R.id.action_day:
+//                try {
+//                    selectData(ymd.parse(ymd.format(new Date())), new Date());
+//                } catch (ParseException e) {
+//                    setAdapter(mDatas);
+//                }
+//                break;
+//            case R.id.action_mouth:
+//                String startDate = "";
+//                try {
+//                    Calendar c = Calendar.getInstance();
+//                    if (c.get(Calendar.MONTH) < 9) {
+//                        startDate = c.get(Calendar.YEAR) + "-0" + (c.get(Calendar.MONTH) + 1) + "-01";
+//                    } else {
+//                        startDate = c.get(Calendar.YEAR) + "-" + (c.get(Calendar.MONTH) + 1) + "-01";
+//                    }
+//                    selectData(ymd.parse(startDate), new Date());
+//                } catch (ParseException e) {
+//                    setAdapter(mDatas);
+//                }
+//                break;
+//            case R.id.action_select:
+//                showSubmitDia();
+//                break;
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * 选择日期对话框
-     */
-    private void showSubmitDia() {
-        final NiftyDialogBuilder submitDia = NiftyDialogBuilder.getInstance(this);
-        View selectDateView = LayoutInflater.from(this).inflate(R.layout.dialog_select_date, null);
-        final DateTimeSelectView ds = (DateTimeSelectView) selectDateView.findViewById(R.id.date_start);
-        final DateTimeSelectView de = (DateTimeSelectView) selectDateView.findViewById(R.id.date_end);
-
-        final Calendar c = Calendar.getInstance();
-        try {
-            startDate = ymdhm.parse(ymdhm.format(new Date()));// 当日00：00：00
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        endDate = new Date();
-
-        submitDia.withTitle("选择查询日期");
-        submitDia.withButton1Text("取消", 0).setButton1Click(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                submitDia.dismiss();
-            }
-        });
-        submitDia.withButton2Text("确定", 0).setButton2Click(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    startDate = ymdhm.parse(ds.getDate());
-                    Log.d(TAG, "开始：" + ds.getDate() + "，结束：" + de.getDate() + "，当前：" + ymdhm.format(new Date()));
-//                    if (ymd.format(new Date()).equals(de.getDate())) {// 结束日期是今天
-//                        endDate = new Date();// 当前时间
+//
+//    /**
+//     * 选择日期对话框
+//     */
+//    private void showSubmitDia() {
+//        final NiftyDialogBuilder submitDia = NiftyDialogBuilder.getInstance(this);
+//        View selectDateView = LayoutInflater.from(this).inflate(R.layout.dialog_select_date, null);
+//        final DateTimeSelectView ds = (DateTimeSelectView) selectDateView.findViewById(R.id.date_start);
+//        final DateTimeSelectView de = (DateTimeSelectView) selectDateView.findViewById(R.id.date_end);
+//
+//        final Calendar c = Calendar.getInstance();
+//        try {
+//            startDate = ymdhm.parse(ymdhm.format(new Date()));// 当日00：00：00
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        endDate = new Date();
+//
+//        submitDia.withTitle("选择查询日期");
+//        submitDia.withButton1Text("取消", 0).setButton1Click(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                submitDia.dismiss();
+//            }
+//        });
+//        submitDia.withButton2Text("确定", 0).setButton2Click(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                try {
+//                    startDate = ymdhm.parse(ds.getDate());
+//                    Log.d(TAG, "开始：" + ds.getDate() + "，结束：" + de.getDate() + "，当前：" + ymdhm.format(new Date()));
+////                    if (ymd.format(new Date()).equals(de.getDate())) {// 结束日期是今天
+////                        endDate = new Date();// 当前时间
+////                    } else {
+////                        endDate = new Date(ymd.parse(de.getDate()).getTime() + 1000 * 60 * 60 * 24 - 1);// 当日23：59：59
+////                    }
+//                    endDate = ymdhm.parse(de.getDate());
+//                    if (endDate.before(startDate)) {
+//                        Toast.makeText(RecordListActivity.this, "开始日期不能大于结束日期", Toast.LENGTH_LONG).show();
 //                    } else {
-//                        endDate = new Date(ymd.parse(de.getDate()).getTime() + 1000 * 60 * 60 * 24 - 1);// 当日23：59：59
+//                        selectData(startDate, endDate);
+//                        setAdapter(mDatas);
+//                        submitDia.dismiss();
 //                    }
-                    endDate = ymdhm.parse(de.getDate());
-                    if (endDate.before(startDate)) {
-                        Toast.makeText(RecordListActivity.this, "开始日期不能大于结束日期", Toast.LENGTH_LONG).show();
-                    } else {
-                        selectData(startDate, endDate);
-                        setAdapter(mDatas);
-                        submitDia.dismiss();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        submitDia.setCustomView(selectDateView, this);// "请选择查询日期"
-        submitDia.withMessage(null).withDuration(400);
-        submitDia.isCancelable(false);
-        submitDia.show();
-    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//        submitDia.setCustomView(selectDateView, this);// "请选择查询日期"
+//        submitDia.withMessage(null).withDuration(400);
+//        submitDia.isCancelable(false);
+//        submitDia.show();
+//    }
 
     private void searchRecord(String result) {
         mDatas = DataSupport.where("room = ? or name = ? or phone = ? or card = ?",
